93f1fcbc348a627e1dde0453a0ccbcd15dd0a3e0
Chris McKenzie <chris@oblong.com>
2011-07-19 15:38:16 -0700

    +injection

---
+++ _inject.js
@@ -0,0 +1,32 @@
+var _inject = (function() {
+
+  function list() {
+    var ret = [], ref;
+
+    for(ref in _inject) {
+      ret.push(ref);
+    }
+
+    return ret;
+  }
+
+  return function(scopeIn, ix){
+
+    if(!scopeIn) { return list(); }
+
+    var scope = ix ? [scopeIn, ix].join('_') : scopeIn;
+
+    return _inject[scope] ? 
+
+      _inject(scopeIn, ( ix || 0 ) + 1) :
+
+      ('self._inject[scope] = ' + function(block) {
+        var result = eval('(' + block + ')');
+
+        return typeof result === 'function' ? 
+          result.call(self._inject[scope]._this) : 
+          result;
+      } + ';self._inject[scope]._this = this;')
+      .replace(/scope/g, '"' + scope + '"');
+  }
+})();
